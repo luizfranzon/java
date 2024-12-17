fa306b6e5d5a65d5c5ed260587bae87f00170bfd
Luiz Fernando Franzon <luizfranzon05@hotmail.com>
2024-12-16 21:46:30 -0300

    add: data types

---
+++ java-1/src/DataTypes/Primitives.java
@@ -0,0 +1,17 @@
+package DataTypes;
+
+public class Primitives {
+    public static void main(String[] args) {
+
+        int maxIntValue = 2147483647;
+        int Age = 21;
+        double salary = 1400.85;
+        float height = 1.80f;
+        char gender = 'M';
+        boolean single = true; //;-; é foda parceiro
+        Long debts = 98745464848648646L;
+
+        System.out.println(2147483647 + 1); //morreu
+
+    }
+}
