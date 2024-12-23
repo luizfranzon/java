02f91e8aa3b2354994b97f3651f80fb041a54d46
Luiz Fernando Franzon <luizfranzon05@hotmail.com>
2024-12-23 13:04:13 -0300

    add: functions

---
+++ java-1/src/Functions.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Functions {
+    public static void main(String[] args) {
+        Scanner scannner = new Scanner(System.in);
+
+        System.out.println("Insira 3 números: ");
+        int firstNumber = scannner.nextInt();
+        int secondNumber = scannner.nextInt();
+        int thirdNumber = scannner.nextInt();
+
+        int biggestNumber = maxNumber(firstNumber, secondNumber, thirdNumber);
+
+        System.out.println("Maior número: " + biggestNumber);
+    }
+
+    public static int maxNumber(int n1, int n2, int n3) {
+
+        if (n1 > n2 && n1 > n3) {
+            return n1;
+        } else if (n2 > n3) {
+            return n2;
+        } else {
+            return n3;
+        }
+
+    }
+}
